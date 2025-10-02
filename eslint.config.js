e765b0661febfe7f9b0e297764fa73edab32c80d
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-10-02 14:09:54 -0300

    chore: add eslint-plugin-simple-import-soft

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
 import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import globals from 'globals'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import reactHooks from 'eslint-plugin-react-hooks'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import react from 'eslint-plugin-react'
 
 export default [
   { ignores: ['dist'] },
@@ -23,6 +24,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -31,6 +33,8 @@ export default [
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
     },
   },
   eslintConfigPrettier,
