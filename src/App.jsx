2e6d4e9bcc83711f2dbd5240e08a3b4e06784d87
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-10-02 16:26:08 -0300

    chore: add shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Button } from '@/components/ui/button'
+
 const App = () => {
   return (
     <div>
-      <h1 className="m-2 text-red-500">FinTrack</h1>
+      <h1 className="m-2 text-red-500">
+        <Button>FinTrack</Button>
+      </h1>
     </div>
   )
 }
