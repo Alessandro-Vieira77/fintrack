664c7117de64ed278d2002dda26d9f335be6627d
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-10-03 10:55:18 -0300

    feat: updated shadcn theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { Button } from '@/components/ui/button'
 const App = () => {
   return (
     <div>
-      <h1 className="m-2 text-red-500">
-        <Button>FinTrack</Button>
+      <h1 className="m-2">
+        <Button className="">FinTrack</Button>
       </h1>
     </div>
   )
