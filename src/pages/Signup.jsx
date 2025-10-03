a647d0e698a1541c9474e8c2185a1f28dda6183c
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-10-03 16:44:33 -0300

    feat: add password input with visibility change

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,3 +1,6 @@
+import { Eye } from 'lucide-react'
+import { EyeOff } from 'lucide-react'
+import { useState } from 'react'
 import { Link } from 'react-router'
 
 import { Button } from '@/components/ui/button'
@@ -12,6 +15,7 @@ import {
 import { Input } from '@/components/ui/input'
 
 function Signup() {
+  const [passwordIsVisible, setPasswordIsVisible] = useState(false)
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-8">
       <Card className="w-[500px]">
@@ -23,7 +27,16 @@ function Signup() {
           <Input placeholder="Digite seu nome" />
           <Input placeholder="Digite seu sobrenome" />
           <Input placeholder="Digite seu e-mail" />
-          <Input type="password" placeholder="Digite sua senha" />
+          <div className="relative">
+            <Input type={passwordIsVisible ? 'text' : 'password'} placeholder="Digite sua senha" />
+            <Button
+              onClick={() => setPasswordIsVisible(value => !value)}
+              variant={'ghost'}
+              className="absolute top-0 right-0 bottom-0 my-auto mr-2 h-8 w-8 leading-0"
+            >
+              {passwordIsVisible ? <EyeOff /> : <Eye />}
+            </Button>
+          </div>
         </CardContent>
         <CardFooter>
           <Button className="w-full">Fazer login</Button>
