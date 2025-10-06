1e2481efdadaad20d288563517798d9c61b026b0
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-10-06 17:15:35 -0300

    feat: ensure password  and password confirmation are the some on signup form

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -28,32 +28,37 @@ import {
 import { Input } from '@/components/ui/input'
 import { api } from '@/lib/axios'
 
-const signupSchema = zod.object({
-  firstName: zod.string().trim().min(1, {
-    message: 'o campo nome é obrigatório',
-  }),
-  lastName: zod.string().trim().min(1, {
-    message: 'o campo sobrenome é obrigatório',
-  }),
-  email: zod
-    .string()
-    .trim()
-    .min(1, {
-      message: 'o campo e-mail é obrigatório',
-    })
-    .email({
-      message: 'o campo e-mail é inválido',
+const signupSchema = zod
+  .object({
+    firstName: zod.string().trim().min(1, {
+      message: 'o campo nome é obrigatório',
     }),
-  password: zod.string().trim().min(6, {
-    message: 'a senha deve ter no mínimo 6 caracteres',
-  }),
-  confirmPassword: zod.string().trim().min(6, {
-    message: 'a confirmação de senha deve ter no mínimo 6 caracteres',
-  }),
-  terms: zod.boolean().refine(value => value === true, {
-    message: 'você deve aceitar nossos termos e condições',
-  }),
-})
+    lastName: zod.string().trim().min(1, {
+      message: 'o campo sobrenome é obrigatório',
+    }),
+    email: zod
+      .string()
+      .trim()
+      .min(1, {
+        message: 'o campo e-mail é obrigatório',
+      })
+      .email({
+        message: 'o campo e-mail é inválido',
+      }),
+    password: zod.string().trim().min(6, {
+      message: 'a senha deve ter no mínimo 6 caracteres',
+    }),
+    confirmPassword: zod.string().trim().min(6, {
+      message: 'a confirmação de senha deve ter no mínimo 6 caracteres',
+    }),
+    terms: zod.boolean().refine(value => value === true, {
+      message: 'você deve aceitar nossos termos e condições',
+    }),
+  })
+  .refine(data => data.password === data.confirmPassword, {
+    message: 'as senhas não coincidem',
+    path: ['confirmPassword'],
+  })
 
 function Signup() {
   const [user, setUser] = useState(null)
