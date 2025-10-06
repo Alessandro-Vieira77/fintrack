5b8b1ee90b8b72cc2344ae24053e6d4e906c0600
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-10-05 21:09:06 -0300

    feat: login of the created form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,104 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router'
+import * as zod from 'zod'
+
+import PasswordInput from '@/components/password-input'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+const loginSchema = zod.object({
+  email: zod.string().trim().email({
+    message: 'o campo e-mail é inválido',
+  }),
+  password: zod.string().trim().min(6, {
+    message: 'a senha deve ter no mínimo 6 caracteres',
+  }),
+})
+
 function Login() {
+  const methods = useForm({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  function onSubmit(values) {
+    console.log(values)
+  }
+
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <h1 className="m-2">Login</h1>
+    <div className="flex min-h-screen max-w-screen flex-col items-center justify-center gap-8">
+      <Form {...methods}>
+        <form onSubmit={methods.handleSubmit(onSubmit)} className="w-full max-w-[500px]">
+          <Card className="w-full max-w-[500px]">
+            <CardHeader className="text-center">
+              <CardTitle className="text-2xl">Entre na sua conta</CardTitle>
+              <CardDescription>Insira seus dados abaixo.</CardDescription>
+            </CardHeader>
+
+            <CardContent className="space-y-4">
+              <FormField
+                control={methods.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite seu e-mail" {...field} autoComplete="off" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={methods.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Senha</FormLabel>
+                    <FormControl>
+                      <PasswordInput placeholder="Digite sua senha" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full cursor-pointer" type="submit">
+                Entrar
+              </Button>
+            </CardFooter>
+          </Card>
+        </form>
+      </Form>
+      <div className="mb-6 flex w-[500px] items-center justify-center gap-1">
+        <p className="text-muted-foreground opacity-50">Ainda não possui uma conta? </p>
+        <Button variant="link" className="p-0" asChild>
+          <Link to={'/signup'} className="text-primary">
+            Crie agora
+          </Link>
+        </Button>
+      </div>
     </div>
   )
 }
